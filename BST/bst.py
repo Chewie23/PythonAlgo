9bec652eaccc2e0f17b0aae574733fdf17899ded
Alan Chu <alan_j_chu@yahoo.com>
2019-03-15 17:34:39 -0700

    Biting the bullet and adding BST

---
+++ BST/bst.py
@@ -0,0 +1,12 @@
+"""
+BST implementation in Python makes literally no sense
+
+Since it's taking higher level data structures to make higher level data
+structures. But if it's for practice, eff it.
+"""
+
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
